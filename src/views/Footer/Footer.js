75d1671e19c68cc5ce35bf58b178ada258871f64
Jmart5564 <Jmart5564@gmail.com>
2022-10-07 16:02:48 -0700

    added linkedin and github links to the footer

---
+++ src/views/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <div>
+      <a href="https://github.com/Jmart5564">
+        <img className='logo' src={`${process.env.PUBLIC_URL}/images/githublogo.png`}></img>
+      </a>
+      <a href="https://www.linkedin.com/in/jessica-martin5564/">
+        <img className='logo' src={`${process.env.PUBLIC_URL}/images/linkedinlogo.png`}></img>
+      </a>
+    </div>
+  );
+}
---
+++ src/views/Header/Header.js
@@ -0,0 +1,10 @@
+// import { NavLink } from 'react-router-dom';
+
+
+export default function Header() {
+  return (
+    <div>
+      {/* <NavLink></NavLink> */}
+    </div>
+  );
+}
