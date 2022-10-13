3fc75eb9ea35b692d1a8713cba3acc2fcaa86075
Jmart5564 <Jmart5564@gmail.com>
2022-10-12 17:05:58 -0700

    email form works, I recieve emails to my gmail account

--- src/views/Connect/Connect.js
+++ src/views/Connect/Connect.js
@@ -1,14 +1,31 @@
-
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
 export default function Connect() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_123nfvi', 'template_lrkbu8w', form.current, 'fgzW0Ll-D-EWG9ESn')
+      .then((result) => {
+        e.target.reset();
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
+
   return (
-    <div>
-      <h1>Connect with Me</h1>
-      <div>
-        <form>
-          
-        </form>
-      </div>
-    </div>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   );
 }
