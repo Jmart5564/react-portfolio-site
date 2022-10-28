6b0e45d503b5eaeebad205621042a2e203ab6a89
Jmart5564 <Jmart5564@gmail.com>
2022-10-28 00:39:09 -0700

    added particle effects to the home page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,23 +2,34 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Text3D } from '@react-three/drei';
-import font1 from '../../fonts/font1.json';
+import { OrbitControls, Text3D, Sparkles, Stars } from '@react-three/drei';
+// import font1 from '../../fonts/font1.json';
+// import font2 from '../../fonts/font2.json';
+// import creepfont from '../../fonts/creepfont.json';
+import codafont from '../../fonts/codafont.json';
+// import combofont from '../../fonts/combofont.json';
 
 function HomeText() { 
   return (
     <mesh>
       <Text3D
-        font={font1}>
+        position={[-4.5, 0, 0]}
+        font={codafont}>
           HELLO WORLD
         <meshNormalMaterial />
       </Text3D>
-      <Text3D
-        font={font1}
-        position={[0, -2, -5]}>
-          Welcome to the void
-        <meshNormalMaterial />
-      </Text3D>
+      <Sparkles 
+        size={3}
+        count={100}
+        scale={[20, 20, 10]}
+        position={[0, 0, 0]}
+        color="#d9cafc"/>
+      <Stars radius={100} 
+        depth={0} 
+        count={1000} 
+        factor={5} 
+        saturation={0} 
+        fade speed={1} />
     </mesh>
   );
 }
